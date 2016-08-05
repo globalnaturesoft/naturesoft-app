3df89153398eb9ea61385b0b613f56ef4f77982b
Luan Pham <luanpm88@gmail.com>
2016-08-04 23:51:55 -0700

    update routes for all engines

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,10 @@ Rails.application.routes.draw do
   
   mount Naturesoft::Core::Engine => "/", as: 'naturesoft'
   
-  if Naturesoft::Core.available?(:slideshows)
-    mount Naturesoft::Slideshows::Engine => "/", as: 'naturesoft_slideshows'
+  Dir.glob(Rails.root.join('engines').to_s + "/*") do |d|
+    eg = d.split(/[\/\\]/).last
+	if eg != "core" and Naturesoft::Core.available?(eg)
+	  mount "Naturesoft::#{eg.camelize}::Engine".constantize => "/", as: 'naturesoft_' + eg
+	end
   end
-  if Naturesoft::Core.available?(:articles)
-    mount Naturesoft::Aricles::Engine => "/", as: 'naturesoft_articles'
-  end
-  if Naturesoft::Core.available?(:banners)
-    mount Naturesoft::Banners::Engine => "/", as: 'naturesoft_banners'
-  end
-  if Naturesoft::Core.available?(:galleries)
-    mount Naturesoft::Galleries::Engine => "/", as: 'naturesoft_galleries'
-  end
-  if Naturesoft::Core.available?(:partners)
-    mount Naturesoft::Partners::Engine => "/", as: 'naturesoft_partners'
-  end
-
 end
