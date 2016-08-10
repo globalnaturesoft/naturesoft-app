5d4d2d7c6e165e3a9a9ed0805d52912429fea64e
Luan Pham <luanpm88@gmail.com>
2016-08-10 00:46:20 -0700

    update form validate js

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,111 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802034310) do
+ActiveRecord::Schema.define(version: 20160810020214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "naturesoft_contacts_contacts", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "company"
-    t.string   "email"
-    t.string   "phone"
+  create_table "naturesoft_articles_articles", force: :cascade do |t|
+    t.string   "image_url"
+    t.string   "title"
+    t.text     "content"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_naturesoft_contacts_contacts_on_user_id", using: :btree
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.boolean  "approved",   default: false
+    t.string   "status",     default: "active"
+    t.integer  "image_id"
+    t.index ["user_id"], name: "index_naturesoft_articles_articles_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_articles_articles_categories", force: :cascade do |t|
+    t.integer  "article_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "naturesoft_articles_categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.integer  "level"
+    t.string   "status",      default: "active"
+    t.index ["user_id"], name: "index_naturesoft_articles_categories_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_articles_images", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "width",      default: 1
+    t.integer  "height",     default: 1
+    t.string   "image_type"
+    t.string   "status",     default: "active"
+    t.integer  "user_id"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.index ["user_id"], name: "index_naturesoft_articles_images_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_articles_parent_categories", force: :cascade do |t|
+    t.integer  "parent_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "naturesoft_banners_banner_types", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "width",       default: 1
+    t.integer  "height",      default: 1
+    t.integer  "user_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "status",      default: "active"
+    t.string   "image_style"
+    t.index ["user_id"], name: "index_naturesoft_banners_banner_types_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_banners_banners", force: :cascade do |t|
+    t.string   "image_url"
+    t.string   "name"
+    t.string   "link_url"
+    t.text     "description"
+    t.integer  "banner_type_id"
+    t.integer  "user_id"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.string   "status",         default: "active"
+    t.index ["banner_type_id"], name: "index_naturesoft_banners_banners_on_banner_type_id", using: :btree
+    t.index ["user_id"], name: "index_naturesoft_banners_banners_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_partners_partners", force: :cascade do |t|
+    t.string   "name"
+    t.string   "logo"
+    t.text     "description"
+    t.string   "website"
+    t.integer  "height"
+    t.integer  "width"
+    t.string   "scale_type"
+    t.string   "status",      default: "active"
+    t.integer  "user_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.index ["user_id"], name: "index_naturesoft_partners_partners_on_user_id", using: :btree
   end
 
   create_table "naturesoft_slideshows_slides", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image"
+    t.string   "status",       default: "active"
     t.integer  "user_id"
     t.integer  "slideshow_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.index ["slideshow_id"], name: "index_naturesoft_slideshows_slides_on_slideshow_id", using: :btree
     t.index ["user_id"], name: "index_naturesoft_slideshows_slides_on_user_id", using: :btree
   end
@@ -44,9 +124,10 @@ ActiveRecord::Schema.define(version: 20160802034310) do
     t.integer  "height"
     t.integer  "width"
     t.string   "image_style"
+    t.string   "status",      default: "active"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.index ["user_id"], name: "index_naturesoft_slideshows_slideshows_on_user_id", using: :btree
   end
 
@@ -64,6 +145,9 @@ ActiveRecord::Schema.define(version: 20160802034310) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "image"
     t.index ["email"], name: "index_naturesoft_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_naturesoft_users_on_reset_password_token", unique: true, using: :btree
   end
