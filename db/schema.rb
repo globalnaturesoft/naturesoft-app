6b0f00c4b2d6b452c71ce449320132c7074cb5e7
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-08-14 23:44:04 -0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810020214) do
+ActiveRecord::Schema.define(version: 20160811032743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,43 +19,32 @@ ActiveRecord::Schema.define(version: 20160810020214) do
     t.string   "image_url"
     t.string   "title"
     t.text     "content"
+    t.string   "status",     default: "active"
+    t.boolean  "approved",   default: false
     t.integer  "user_id"
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
-    t.boolean  "approved",   default: false
-    t.string   "status",     default: "active"
-    t.integer  "image_id"
     t.index ["user_id"], name: "index_naturesoft_articles_articles_on_user_id", using: :btree
   end
 
   create_table "naturesoft_articles_articles_categories", force: :cascade do |t|
-    t.integer  "article_id"
     t.integer  "category_id"
+    t.integer  "article_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["article_id"], name: "index_naturesoft_articles_articles_categories_on_article_id", using: :btree
+    t.index ["category_id"], name: "index_naturesoft_articles_articles_categories_on_category_id", using: :btree
   end
 
   create_table "naturesoft_articles_categories", force: :cascade do |t|
     t.string   "name"
+    t.integer  "level"
     t.text     "description"
+    t.string   "status",      default: "active"
     t.integer  "user_id"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.integer  "level"
-    t.string   "status",      default: "active"
     t.index ["user_id"], name: "index_naturesoft_articles_categories_on_user_id", using: :btree
-  end
-
-  create_table "naturesoft_articles_images", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "width",      default: 1
-    t.integer  "height",     default: 1
-    t.string   "image_type"
-    t.string   "status",     default: "active"
-    t.integer  "user_id"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.index ["user_id"], name: "index_naturesoft_articles_images_on_user_id", using: :btree
   end
 
   create_table "naturesoft_articles_parent_categories", force: :cascade do |t|
@@ -65,30 +54,70 @@ ActiveRecord::Schema.define(version: 20160810020214) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "naturesoft_banners_banner_types", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "width",       default: 1
-    t.integer  "height",      default: 1
-    t.integer  "user_id"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "status",      default: "active"
-    t.string   "image_style"
-    t.index ["user_id"], name: "index_naturesoft_banners_banner_types_on_user_id", using: :btree
-  end
-
   create_table "naturesoft_banners_banners", force: :cascade do |t|
     t.string   "image_url"
     t.string   "name"
     t.string   "link_url"
     t.text     "description"
-    t.integer  "banner_type_id"
+    t.string   "status",      default: "active"
     t.integer  "user_id"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.string   "status",         default: "active"
-    t.index ["banner_type_id"], name: "index_naturesoft_banners_banners_on_banner_type_id", using: :btree
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.index ["user_id"], name: "index_naturesoft_banners_banners_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_contacts_contacts", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "address"
+    t.string   "phone"
+    t.text     "message"
+    t.string   "subject"
+    t.string   "status",     default: "active"
+    t.integer  "user_id"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.index ["user_id"], name: "index_naturesoft_contacts_contacts_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_galleries_galleries", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "height"
+    t.integer  "width"
+    t.string   "image_style"
+    t.string   "status",      default: "active"
+    t.integer  "user_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.index ["user_id"], name: "index_naturesoft_galleries_galleries_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_galleries_images", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "image"
+    t.string   "status",      default: "active"
+    t.integer  "user_id"
+    t.integer  "gallery_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.index ["gallery_id"], name: "index_naturesoft_galleries_images_on_gallery_id", using: :btree
+    t.index ["user_id"], name: "index_naturesoft_galleries_images_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_newsletters_newsletters", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "naturesoft_options", force: :cascade do |t|
+    t.string   "category"
+    t.string   "name"
+    t.text     "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "naturesoft_partners_partners", force: :cascade do |t|
@@ -104,6 +133,118 @@ ActiveRecord::Schema.define(version: 20160810020214) do
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
     t.index ["user_id"], name: "index_naturesoft_partners_partners_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_products_categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "level"
+    t.string   "status",      default: "active"
+    t.integer  "user_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.index ["user_id"], name: "index_naturesoft_products_categories_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_products_categories_products", force: :cascade do |t|
+    t.integer  "category_id"
+    t.integer  "product_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_naturesoft_products_categories_products_on_category_id", using: :btree
+    t.index ["product_id"], name: "index_naturesoft_products_categories_products_on_product_id", using: :btree
+  end
+
+  create_table "naturesoft_products_images", force: :cascade do |t|
+    t.string   "image_url"
+    t.boolean  "is_main",    default: false
+    t.integer  "product_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.index ["product_id"], name: "index_naturesoft_products_images_on_product_id", using: :btree
+  end
+
+  create_table "naturesoft_products_manufacturers", force: :cascade do |t|
+    t.string   "image_url"
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "address"
+    t.text     "description"
+    t.string   "status",      default: "active"
+    t.integer  "user_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.index ["user_id"], name: "index_naturesoft_products_manufacturers_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_products_parent_categories", force: :cascade do |t|
+    t.integer  "parent_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_naturesoft_products_parent_categories_on_category_id", using: :btree
+  end
+
+  create_table "naturesoft_products_products", force: :cascade do |t|
+    t.string   "name"
+    t.string   "code"
+    t.decimal  "new_price"
+    t.decimal  "old_price"
+    t.integer  "discount"
+    t.decimal  "quantity"
+    t.string   "unit"
+    t.string   "tags"
+    t.string   "status",            default: "active"
+    t.boolean  "is_show",           default: false
+    t.text     "short_description"
+    t.text     "description"
+    t.integer  "manufacturer_id"
+    t.integer  "image_id"
+    t.integer  "user_id"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.index ["image_id"], name: "index_naturesoft_products_products_on_image_id", using: :btree
+    t.index ["manufacturer_id"], name: "index_naturesoft_products_products_on_manufacturer_id", using: :btree
+    t.index ["user_id"], name: "index_naturesoft_products_products_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_projects_categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "level"
+    t.string   "status",      default: "active"
+    t.integer  "user_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.index ["user_id"], name: "index_naturesoft_projects_categories_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_projects_images", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "image"
+    t.string   "status",      default: "active"
+    t.boolean  "is_main",     default: false
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.index ["project_id"], name: "index_naturesoft_projects_images_on_project_id", using: :btree
+    t.index ["user_id"], name: "index_naturesoft_projects_images_on_user_id", using: :btree
+  end
+
+  create_table "naturesoft_projects_projects", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "website"
+    t.string   "status",      default: "active"
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.index ["category_id"], name: "index_naturesoft_projects_projects_on_category_id", using: :btree
+    t.index ["user_id"], name: "index_naturesoft_projects_projects_on_user_id", using: :btree
   end
 
   create_table "naturesoft_slideshows_slides", force: :cascade do |t|
